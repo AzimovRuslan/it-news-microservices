1183371845e6edcadd051d0c14e07826c1be77da
AzimovRuslan <55226276+AzimovRuslan@users.noreply.github.com>
2024-02-27 20:37:06 +0300

    downloaded first version it news reader

---
+++ it-news-reader-microservice/src/main/java/com/example/itnewsreadermicroservice/ItNewsReaderMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.itnewsreadermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItNewsReaderMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItNewsReaderMicroserviceApplication.class, args);
+    }
+
+}
